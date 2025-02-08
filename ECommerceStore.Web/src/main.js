c33265230cf90f14e1a5115fa253ae193d670a40
Dorian Jarosz <deltian1989@gmail.com>
2025-02-08 16:19:48 +0100

    Added npm support with stylesheets and JS scripts bundling

---
+++ ECommerceStore.Web/src/main.js
@@ -0,0 +1,2 @@
+﻿import "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./styles.css";
---
+++ ECommerceStore.Web/vite.config.js
@@ -0,0 +1,16 @@
+﻿import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        outDir: "wwwroot/build",
+        rollupOptions: {
+            input: {
+                main: "./src/main.js"
+            },
+            output: {
+                entryFileNames: "bundle.js",
+                assetFileNames: "styles.css"
+            }
+        }
+    }
+});
