ff9863a3faf95287969e77122c3d9b21d9836f9f
Deltian1989 <deltian1989@gmail.com>
2025-02-21 16:03:02 +0100

    Creating a product slider using swiper.js

---
+++ ECommerceStore.Web/wwwroot/js/app.js
@@ -0,0 +1,21 @@
+﻿
+var App = {
+    Common: {
+
+    },
+    Core: {
+
+    },
+    Pages: {
+        HomePage: {
+            init: function () {
+                App.Utils.Widgets.createProductSwiper('featured-products-slider');
+            }
+        }
+    },
+    Utils: {
+        Widgets: {
+
+        }
+    }
+};
---
+++ ECommerceStore.Web/wwwroot/js/utils/widgets.js
@@ -0,0 +1,27 @@
+﻿
+App.Utils.Widgets.createProductSwiper = function (productSwiperId) {
+
+    if (this.productSwiper) {
+        this.productSwiper.destroy(true, true); // Destroy existing instance before reinitializing
+    }
+
+    this.productSwiper = new Swiper('.' + productSwiperId, {
+        slidesPerView: 4, // Number of visible slides
+        spaceBetween: 20, // Space between slides
+        loop: true, // Enable infinite scrolling
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        breakpoints: {
+            320: { slidesPerView: 1 },
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+            1280: { slidesPerView: 4 }
+        }
+    });
+}
