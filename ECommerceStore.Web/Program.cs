d97f483a19f518a8b6b7221a195b5fef880ddc7a
Deltian1989 <deltian1989@gmail.com>
2025-03-05 14:49:05 +0100

    Added localization for the navbar (for english and polish language)

--- ECommerceStore.Web/Program.cs
+++ ECommerceStore.Web/Program.cs
@@ -1,6 +1,7 @@
 using ECommerceStore.Web;
 using ECommerceStore.Web.Components;
 using Microsoft.Extensions.FileProviders;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,12 @@ builder.AddRedisOutputCache("cache");
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var culture = new CultureInfo("pl-PL");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
 var app = builder.Build();
 
@@ -35,6 +42,14 @@ app.UseStaticFiles(new StaticFileOptions
         Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
     RequestPath = "/lib"
 });
+
+var supportedCultures = new[] { "en", "pl" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
