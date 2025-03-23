9c28db9465967bfa66b9aa5adfcb6661a0b8716a
Dorian Jarosz <deltian1989@gmail.com>
2025-03-23 16:16:49 +0100

    Rebuilding the product sliders in the home page

---
+++ ECommerceStore.Web/Models/DiscountedProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceStore.Web.Models
+{
+    public class DiscountedProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int DiscountPercentage { get; set; }
+        public decimal PriceWithDiscount { get; set; }
+    }
+}
